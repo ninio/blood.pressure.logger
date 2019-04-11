2426c60c4c5e7ccb7277dd536baeedfaf8af35fb
Nikolay Ninarski <n.ninarski@up2.nl>
2019-04-11 18:09:47 +0300

    Added styling to the list view

--- components/MeasurementListItem.js
+++ components/MeasurementListItem.js
@@ -12,21 +12,34 @@ export default class MeasurementListItem extends React.Component {
 		if ( measurementData ) {
 			return (
 				<Card>
-					<View>
+					<View style={ styles.measurementDetails }>
+						<View style={ styles.measurementData }>
+							<View style={ styles.measurementLow }>
+								<Text>low </Text>
+								<Text style={ styles.measurementLowText }>
+									{ measurementData.low }
+								</Text>
+							</View>
+							<View style={ styles.measurementHigh }>
+								<Text>high </Text>
+								<Text style={ styles.measurementHighText }>
+									{ measurementData.high }
+								</Text>
+							</View>
+							<View style={ styles.measurementPulse }>
+								<Text>
+									pulse
+								</Text>
+								<Text style={ styles.measurementPulseText }>
+									{ measurementData.pulse }
+								</Text>
+							</View>
+						</View>
 						<View style={ styles.measurementDate }>
-							<Text style={ { color: '#09c', flex: 1 } }>
+							<Text style={ styles.measurementDateText }>
 								{ DateTime.fromISO( date ).toLocaleString() }
 							</Text>
 						</View>
-						<Text style={ { flex: 1 } }>
-							{ measurementData.low }
-						</Text>
-						<Text style={ { flex: 1 } }>
-							{ measurementData.high }
-						</Text>
-						<Text style={ { flex: 1 } }>
-							{ measurementData.pulse }
-						</Text>
 					</View>
 				</Card>
 			);
@@ -37,9 +50,46 @@ export default class MeasurementListItem extends React.Component {
 }
 
 const styles = StyleSheet.create( {
+	measurementDetails: {
+		flexDirection: 'row'
+	},
 	measurementDate: {
-		color: '#09c',
+		flex: 1,
+		paddingRight: 12,
+		paddingBottom: 12
+	},
+	measurementData: {
+		flex: 5,
+		flexDirection: 'row'
+	},
+	measurementDateText: {
+		color: '#999',
+		textAlign: 'right'
+	},
+	measurementLow: {
+		height: 64,
+		flex: 1,
+		paddingLeft: 12
+	},
+	measurementHigh: {
+		height: 64,
 		flex: 1
+	},
+	measurementPulse: {
+		height: 64,
+		flex: 1
+	},
+	measurementLowText: {
+		textAlign: 'center',
+		fontSize: 24
+	},
+	measurementHighText: {
+		textAlign: 'center',
+		fontSize: 32
+	},
+	measurementPulseText: {
+		textAlign: 'center',
+		fontSize: 24,
 	},
 	toolbar: {
 		backgroundColor: '#aa3333',
