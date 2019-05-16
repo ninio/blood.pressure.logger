893d0e3b46b51b7173b8d28c0baa09fb4dc431cc
Nikolay Ninarski <n.ninarski@up2.nl>
2019-05-16 23:37:27 +0300

    Added webview with tesseract in order to avoid ejecting from expo

--- containers/AddMeasurementOCR.js
+++ containers/AddMeasurementOCR.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { TouchableOpacity, StyleSheet, Button, Text, View } from 'react-native';
+import { TouchableOpacity, WebView, StyleSheet, Button, Text, View } from 'react-native';
 
 import { Camera, Permissions } from 'expo';
 // import { ImagePicker } from 'expo';
@@ -10,6 +10,42 @@ import { Ionicons } from '@expo/vector-icons';
 
 import storage from './../middleware/localStorage.js';
 
+
+const HTML = `
+<div id="thediv">yooo</div>
+<div id="imagediv">image here</div>
+
+<script src='https://cdn.rawgit.com/naptha/tesseract.js/1.0.10/dist/tesseract.js'></script>
+<script>
+var base64 = "";
+var output = "";
+function resetImage() {
+  base64 = "";
+}
+function addImageData(data) {
+  base64 = base64 + data;
+}
+function writeResult(text) {
+  output = output + '<br/>' + text;
+  document.getElementById('thediv').innerHTML = output;
+}
+
+function run(width, height) {
+  width = 500;
+  height = 500;
+  document.getElementById('imagediv').innerHTML = "<img id='theimage' src='" + base64 + "' width=" + width + " height=" + height + "/>";
+
+  setTimeout(function () {
+    var theimage = document.getElementById('theimage');
+    writeResult('loading: ' + theimage);
+    Tesseract.recognize(theimage)
+         .progress(function  (p) { writeResult('progress ' + JSON.stringify(p))    })
+         .catch(function (e) {  writeResult('error ' + JSON.stringify(e)) })
+         .then(function (result) { writeResult('result ' + JSON.stringify(result)) });
+  }, 500);
+}
+</script>
+`;
 
 const initialMeasurements = {
 	low: 80,
@@ -74,6 +110,29 @@ export default class AddMeasurement extends React.Component {
 		}
 	}
 
+	_takePhoto = async () => {
+		let photo = await this.camera.takePictureAsync( {
+			base64: true,
+			quality: 0,
+		} );
+		//console.log(photo.base64.substring(0, 20));
+		//"data:image/jpg;base64,' + photo.base64 + '",
+		//' + photo.width + ', ' + photo.height + '
+		//this.webview.injectJavaScript('run("data:image/jpg;base64,' + photo.base64.substring(0, 20) + '", ' + photo.width + ', ' + photo.height + ')');
+
+		this.webview.injectJavaScript( 'resetImage()' );
+
+		let data = "data:image/jpg;base64," + photo.base64;
+		let index = 0;
+		const BUFFER_SIZE = 10000;
+		while ( index < data.length ) {
+			this.webview.injectJavaScript( 'addImageData("' + data.substring( index, Math.min( index + BUFFER_SIZE, data.length ) ) + '")' );
+			index += BUFFER_SIZE;
+		}
+
+		this.webview.injectJavaScript( 'run(' + photo.width + ', ' + photo.height + ')' );
+	}
+
 	render () {
 		const { hasCameraPermission } = this.state;
 		if ( hasCameraPermission === null ) {
@@ -84,6 +143,12 @@ export default class AddMeasurement extends React.Component {
 			return (
 				<View style={ { flex: 1 } }>
 					<View style={ { flex: 1 } }>
+						<WebView
+							javaScriptEnabled={ true }
+							ref={ ref => { this.webview = ref; } }
+							source={ { html: HTML } }
+							style={ { marginTop: 20, height: 30 } }
+						/>
 						<Camera style={ { flex: 1 } } type={ this.state.type }>
 							<View
 								style={ {
@@ -107,6 +172,18 @@ export default class AddMeasurement extends React.Component {
 									<Text
 										style={ { fontSize: 18, marginBottom: 10, color: 'white' } }>
 										{ ' ' }Flip{ ' ' }
+									</Text>
+								</TouchableOpacity>
+								<TouchableOpacity
+									style={ {
+										flex: 0.1,
+										alignSelf: 'flex-end',
+										alignItems: 'center',
+									} }
+									onPress={ () => this._takePhoto() }>
+									<Text
+										style={ { fontSize: 18, marginBottom: 10, color: 'white' } }>
+										{ ' ' }Take{ ' ' }
 									</Text>
 								</TouchableOpacity>
 							</View>
