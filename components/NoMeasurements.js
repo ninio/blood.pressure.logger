b8ba2a4207f021ef5b40f7e5b428c91e86a91215
Nikolay Ninarski <n.ninarski@up2.nl>
2019-08-17 11:09:37 +0300

    Added localization. Added NoMeasurement component with localization.

---
+++ components/NoMeasurements.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+import * as Localization from 'expo-localization';
+import i18n from 'i18n-js';
+
+const translations = {
+	'bg-BG': {
+		noMeasurementsText: `Все още нямате измервания.`,
+		initialInstructionText: `Измерете кръвното си налягане и натиснете '+' за да го запишете.`
+	},
+	'en-US': {
+		noMeasurementsText: `You don't have any measurements yet.`,
+		initialInstructionText: `Measure your blood pressure and press '+' to add it.`
+	},
+	'en-UK': {
+		noMeasurementsText: `You don't have any measurements yet.`,
+		initialInstructionText: `Measure your blood pressure and press '+' to add it.`
+	}
+}
+
+i18n.fallbacks = true;
+i18n.translations = translations;
+i18n.locale = Localization.locale;
+
+
+export default ( { style } ) => (
+	<View style={ style }>
+		<Text>{ i18n.t( 'noMeasurementsText' ) }</Text>
+		<Text>{ i18n.t( 'initialInstructionText' ) }</Text>
+	</View>
+);
